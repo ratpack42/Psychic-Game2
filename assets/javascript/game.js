525bd8455f5d649b081572d2d2201cf98e9ae4f3
Craig Jackson <craigjackson@Craigs-MacBook-Pro.local>
2017-06-30 16:00:11 -0500

    Finalmente

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,8 @@
 //Global Scope Vars Available//
 //Letter Choices Available//
 var compChoice = ['a', 'b', 'c', 'd,', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-	
+
+
 
 //Scoreboard//
 var wins = 0;
@@ -9,41 +10,59 @@ var losses = 0;
 var guessesLeft = 9;
 var guessedLetters = [];
 var userGuess = null;
-
+var randomNumber = Math.floor(Math.random() * compChoice.length);
+console.log(randomNumber);
 //Let's the Computer Select Random Letters//
-var compGuess = compChoice[Math.floor(Math.random() * compChoice.length)];
-console.log("This is the Answer" + compGuess);
+
+
 
 //Listens for event
 	document.onkeyup = function(event) {
+		compGuess = compChoice[randomNumber];
+		console.log(compGuess);
 //Change userGuess to Event.key	
-	 userGuess = event.key;
+		userGuess = event.key;
+		console.log(userGuess);
+//Make guesses go down by 1
+		guessesLeft = guessesLeft - 1;
 	
-// When user presses a key, it records it and saves to userGuess
-	var soFar = document.getElementById("soFar");
-	soFar.innerHTML = "Your guesses so far: " + userGuess;
 
-//Lose Condition is when userGuesses equals 9
-	guessedLetters.push(userGuess);
-	if (guessedLetters.length === 9) {
-		alert("Your not a Psychic After All");
-	} else {
-//Add additional letters to Your guesses so far via array using push function
-//Have to create additional element
-		
-		soFar.appendChild = guessedLetters;
-	}
-//Win Coditions - When computer choice equals user condition
-	if (compGuess === userGuess) {
-		alert("YOU WIN");
-	}
 
+	//Lose Condition is when userGuesses equals 9
+		guessedLetters.push(userGuess);
+		if (guessesLeft === 0) {
+			guessesLeft = 9;
+			guessedLetters = [];
+			losses = losses +1;
+			document.getElementById("losses").innerHTML ="Losses: " +losses;
+	//Resets picks random letter again
+			randomNumber = Math.floor(Math.random() * compChoice.length);
+			// var compGuess = compChoice[Math.floor(Math.random() * compChoice.length)];	
+		}
+
+
+	// When user presses a key, it records it and saves to userGuess
+		var soFar = document.getElementById("soFar");
+		soFar.innerHTML = "Your guesses so far: " + guessedLetters;
+
+	//Make Guesses Left appear in HTML
+		document.getElementById("guessLeft").innerHTML ="Guesses Left: " +guessesLeft;
+
+	//Win Coditions - When computer choice equals user condition
+		if (compGuess === userGuess) {
+			//("YOU WIN");
+			wins = wins + 1;
+			document.getElementById("wins").innerHTML ="Wins: " + wins;
+			guessesLeft= 9;
+			guessedLetters = [];
+	//Resets picks random letter
+			randomNumber = Math.floor(Math.random() * compChoice.length);
+
+		}
 
 }
 
 
-
-//Allows the user 9 guesses
 
 
 
